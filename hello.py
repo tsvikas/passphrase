192da4f1c6191b1115a72ddd018bd277563594fd
Tsvika S <tsvika@github>
2024-12-16 18:07:57 +0200

    add passphrase logic

--- hello.py
+++ hello.py
@@ -1,5 +1,29 @@
+import string
+import random
+
+import wordfreq
+
+
+def get_wordlist() -> list[str]:
+    short_words = {}
+    for word in wordfreq.iter_wordlist("en"):
+        assert word == word.lower()
+        if not set(word).issubset(string.ascii_lowercase):
+            continue
+        if len(word) >= 3 and word[:3] not in short_words:
+            short_words[word[:3]] = word
+            if len(short_words) >= 1024:
+                break
+    return short_words
+
+
+WORDLIST = list(get_wordlist().values())
+
+
 def main():
-    print("Hello from passphrase!")
+    for _ in range(8):
+        words = random.choices(WORDLIST, k=6)
+        print(" ".join(words))
 
 
 if __name__ == "__main__":
