b557515612a4b35816adbe1fdf20db9acea7682e
Tsvika S <tsvika@github>
2024-12-17 17:22:41 +0200

    minor module docstring changes

--- passphrase.py
+++ passphrase.py
@@ -35,19 +35,20 @@ Passphrase Generator with Memorable Word Selection.
 This module helps create secure passphrases.
 See [this XKCD comic](https://xkcd.com/936/) about passphrases.
 To correctly use a passphrase, you need to make sure your selections are
-chosen at random (i.e. don't pick them yourself) and that you use enought
+chosen at random (i.e. don't pick them yourself) and that you use enough
 bits of entropy.
 
-It draws from the most frequent words in the English language.
+This module draws from the most frequent words in the English language.
 This ensures memorability by choosing common, easily recognized words.
 
-It limits the wordlist to words with unique 3-letter prefixes.
-The unique prefix approach enables users to potentially shorten the passphrase
-while maintaining readability, as each prefix corresponds to a distinct word.
+It also limits the wordlist to words with unique 3-letter prefixes.
+This enables users to shorten their passphrase while maintaining
+readability, as each prefix corresponds to a distinct word.
 
-This is instead of using
-[eff_large_wordlist](https://www.eff.org/document/passphrase-wordlists)
+This is instead of using the
+[eff_large wordlist](https://www.eff.org/document/passphrase-wordlists)
 which contains many obscure or hard to spell words.
+
 Note that this means that the wordlist is shorter, and a longer phrase is
 needed to reach the same entropy.
 
