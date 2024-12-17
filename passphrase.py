b7675bbd6c2157988d27e08c8343fbcd96746cc9
Tsvika S <tsvika@github>
2024-12-17 12:36:37 +0200

    remove redundent check

--- passphrase.py
+++ passphrase.py
@@ -19,8 +19,6 @@ def get_wordlist() -> list[str]:
     wordlist_size = 1024
     short_words = {}
     for word in wordfreq.iter_wordlist("en"):
-        if word != word.lower():
-            continue
         if not set(word).issubset(string.ascii_lowercase):
             continue
         if len(word) >= prefix_size and word[:prefix_size] not in short_words:
