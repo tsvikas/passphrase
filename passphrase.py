a3a04a47065d77ea1c6b9a009354943ee9c78b2d
Tsvika S <tsvika@github>
2024-12-17 12:19:19 +0200

    refactor - give names to values

--- passphrase.py
+++ passphrase.py
@@ -15,14 +15,16 @@ import wordfreq
 
 
 def get_wordlist() -> list[str]:
+    prefix_size = 3
+    wordlist_size = 1024
     short_words = {}
     for word in wordfreq.iter_wordlist("en"):
         assert word == word.lower()
         if not set(word).issubset(string.ascii_lowercase):
             continue
-        if len(word) >= 3 and word[:3] not in short_words:
-            short_words[word[:3]] = word
-            if len(short_words) >= 1024:
+        if len(word) >= prefix_size and word[:prefix_size] not in short_words:
+            short_words[word[:prefix_size]] = word
+            if len(short_words) >= wordlist_size:
                 break
     return short_words
 
